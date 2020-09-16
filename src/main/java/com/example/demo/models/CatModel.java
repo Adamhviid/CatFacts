f500e05e0e7e39cd442639ab173be236cfaaf2e9
Adam <adam5596@stud.kea.dk>
2020-09-16 15:20:31 +0200

    Cleaned up document

--- src/main/java/com/example/demo/models/CatModel.java
+++ src/main/java/com/example/demo/models/CatModel.java
@@ -6,7 +6,6 @@ public class CatModel implements Comparable<CatModel>{
 
     private String text;
     private Date createdAt;
-    private Date updatedAt;
 
     public CatModel(){
     }
@@ -32,27 +31,6 @@ public class CatModel implements Comparable<CatModel>{
         public String getText() {
         return text;
     }
-/*
-    public void setText(String text) {
-        this.text = text;
-    }
-
-    public Date getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(Date createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public Date getUpdatedAt() {
-        return updatedAt;
-    }
-
-    public void setUpdatedAt(Date updatedAt) {
-        this.updatedAt = updatedAt;
-    }
- */
 }
 
 
